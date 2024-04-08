b1b1ca716e0a12cabc37fef3954115539972cd2f
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-08 12:13:38 +0600

    Brute Force Solution added

---
+++ 904. Fruit Into Baskets.py
@@ -0,0 +1,14 @@
+# Brute Force Approch
+# Time Complexity O(n^2)
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        maxlen = 0
+        for i in range(len(fruits)):
+            hashSet = set()
+            for j in range(i, len(fruits)):
+                hashSet.add(fruits[j])
+                if len(hashSet) <= 2:
+                    maxlen = max(maxlen, j - i + 1)
+                else:
+                    break
+        return maxlen
